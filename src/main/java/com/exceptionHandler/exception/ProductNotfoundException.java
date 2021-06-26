801e2afad3a472d7537657bf54692902029eb71c
somwanshi.dnyaneshwar@gmail.com <Mahatma@352>
2021-06-26 11:44:21 +0530

    first commit

---
+++ src/main/java/com/exceptionHandler/exception/ProductNotfoundException.java
@@ -0,0 +1,6 @@
+package com.exceptionHandler.exception;
+
+public class ProductNotfoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/exceptionHandler/model/Product.java
@@ -0,0 +1,10 @@
+package com.exceptionHandler.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+	private String id;
+	   private String name;
+
+}
